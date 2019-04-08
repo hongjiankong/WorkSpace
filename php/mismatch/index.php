dc5eba3771e9bc8249f3b0f98ac1aa972c003ae3
Ray <hongjian.kong@vip.163.com>
2019-04-08 15:48:21 +0800

    test change url

--- php/mismatch/index.php
+++ php/mismatch/index.php
@@ -19,6 +19,7 @@
 
   // Connect to the database 
   $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME); 
+  
 
   // Retrieve the user data from MySQL
   $query = "SELECT user_id, first_name, picture FROM mismatch_user WHERE first_name IS NOT NULL ORDER BY join_date DESC LIMIT 5";
